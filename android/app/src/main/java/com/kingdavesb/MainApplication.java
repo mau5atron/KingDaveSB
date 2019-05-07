3e2f544c8901df9cc584d48892abb6a3684822e6
Gabriel Betancourt <gabrielbetancourt96@outlook.com>
2019-05-07 12:17:44 -0400

    added linear gradient to android

--- android/app/src/main/java/com/kingdavesb/MainApplication.java
+++ android/app/src/main/java/com/kingdavesb/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.BV.LinearGradient.LinearGradientPackage; // linear gradient
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactNativeConfigPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new LinearGradientPackage()
       );
     }
 
